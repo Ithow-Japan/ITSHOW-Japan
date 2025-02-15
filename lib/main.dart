a16de52550a38f86ea461c0aedf8a0d0db8bfd0b
lseongmi <w2307@e-mirim.hs.kr>
2025-02-16 01:57:43 +0900

    Feat : Category 개발

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:harugo/Widgets/categori.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,10 +28,69 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-        body: CircleAvatar(
-          child: Image.asset(
-            'assets/harugo.png',
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/yumi.png'),
+                    radius: 60,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '유미',
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                      Text(
+                        'berry04283',
+                        style:
+                            TextStyle(fontSize: 12, color: Color(0xFF9D9D9D)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Categori(
+                  iconColor: Color(0xFF0071CE),
+                  name: '학습한 문장 수',
+                  time: '1h 30m',
+                  caicon: Icons.chat_outlined,
+                ),
+                Categori(
+                  iconColor: Color(0xFF00883F),
+                  name: '누적 출석일',
+                  time: '30d',
+                  caicon: Icons.calendar_today,
+                ),
+                Categori(
+                  iconColor: Color(0xFFC90000),
+                  name: '연속 출석일',
+                  time: '30d',
+                  caicon: Icons.fireplace_outlined,
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
